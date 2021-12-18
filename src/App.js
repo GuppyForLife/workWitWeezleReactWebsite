8dc5db2efcb81cfb13ff1aeb7270ce65ff4becd2
Matthew Jovenal <matthewjovenal1@outlook.com>
2021-12-17 21:18:23 -0700

    Live site attempt 1

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter basename={window.location.pathname || ''}>
+    <BrowserRouter>
       <div className="App">
         <Main />
       </div>
